1eca5aeaf4c5bb5a9a97850b86185c61d2d3e159
Jenan Morrar <jenanmorrar06@gmail.com>
2021-10-08 23:09:37 +0300

    version 1

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void show(View view){
+        editText = findViewById(R.id.editTextTextPersonName);
+        textView = findViewById(R.id.textView3);
+        textView.setText("What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+    }
+}
